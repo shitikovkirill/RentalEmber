35a4ca0fcb50dfd5bd8de56ae15782f7454acf92
Shitikov Kyrylo <kshy@isddesign.com>
2017-08-16 23:49:33 +0300

    Add empty test

--- tests/acceptance/list-rentals-test.js
+++ tests/acceptance/list-rentals-test.js
@@ -3,10 +3,20 @@ import moduleForAcceptance from 'super-rentals/tests/helpers/module-for-acceptan
 
 moduleForAcceptance('Acceptance | list rentals');
 
-test('visiting /', function(assert) {
-  visit('/');
+test('should show rentals as the home page', function (assert) {
+});
 
-  andThen(function() {
-    assert.equal(currentURL(), '/');
-  });
+test('should link to information about the company.', function (assert) {
+});
+
+test('should link to contact information.', function (assert) {
+});
+
+test('should list available rentals.', function (assert) {
+});
+
+test('should filter the list of rentals by city.', function (assert) {
+});
+
+test('should show details for a selected rental', function (assert) {
 });
