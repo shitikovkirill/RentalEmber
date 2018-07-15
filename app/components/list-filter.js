b2099426fd16af7f7fb034fa83dc8894f760afb4
Shitikov Kirill <sh.kiruh@gmail.com>
2018-07-15 15:51:57 +0300

    Find by bedrooms

--- app/components/list-filter.js
+++ app/components/list-filter.js
@@ -3,19 +3,28 @@ import Component from '@ember/component';
 export default Component.extend({
   classNames: ['list-filter'],
   storey: 0,
+  bedrooms: 0,
 
   init() {
     this._super(...arguments);
     this.get('filter')({}).then((allResults) => this.set('results', allResults.results));
   },
 
+  getRentalQuery(){
+    let data = {};
+    if(this.get('storey')){
+      data['storey'] = this.get('storey');
+    }
+
+    if(this.get('bedrooms')){
+      data['bedrooms'] = this.get('bedrooms');
+    }
+    return data;
+  },
+
   actions: {
     handleFilterEntry() {
-      let data = {};
-      if(this.get('storey')){
-        data['storey'] = this.get('storey');
-      }
-
+      let data = this.getRentalQuery();
       let filterAction = this.get('filter');
       filterAction(data).then((resultsObj) => {
         if (data && resultsObj.query === data) {
