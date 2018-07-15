078374fd450d8587926af24a973e0526059f3cdf
Shitikov Kirill <sh.kiruh@gmail.com>
2018-07-15 15:26:40 +0300

    Rename filter function

--- app/controllers/rentals.js
+++ app/controllers/rentals.js
@@ -2,7 +2,7 @@ import Controller from '@ember/controller';
 
 export default Controller.extend({
   actions: {
-    filterByCity(param) {
+    filterAll(param) {
       if (param !== '') {
         return this.get('store').query('rental', { city: param }).then((filteredResults) => {
           return { query: param, results: filteredResults };
