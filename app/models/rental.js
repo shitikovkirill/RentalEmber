2769b9284c51330a25acfc53674386c4b340e3cc
Shitikov Kirill <sh.kiruh@gmail.com>
2018-07-15 15:44:58 +0300

    Delete some fields

--- app/models/rental.js
+++ app/models/rental.js
@@ -2,11 +2,12 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   title: DS.attr(),
+
   storey: DS.attr('number'),
-  owner: DS.attr(),
-  city: DS.attr(),
+  bedrooms: DS.attr('number'),
+
   image: DS.attr(),
-  bedrooms: DS.attr(),
+
   description: DS.attr(),
   categories: DS.hasMany('category'),
 });
